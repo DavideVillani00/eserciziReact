35f4291beac8ad1a4db3e48f58686fa4dfe7e871
DavideVillani00 <villani.dado@gmail.com>
2024-11-23 02:08:15 +0100

    fatto la logica del daFare/fatto

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,59 +1,39 @@
 import { useState } from "react";
 import "./App.css";
+import { Item } from "./assets/components/Item";
 
 function App() {
-  const [idtodo, setIdTodo] = useState("0");
+  const [idtodo, setIdTodo] = useState(1);
   const [inputValue, setInputValue] = useState("");
-  const [todolist, setTodolist] = useState([]);
-
+ const [todolist, setTodolist] = useState([]);
+// aggiornamento al cambio di input
   const handleChange = (e) => {
     setInputValue(e.target.value);
-    console.log(inputValue);
+    
   };
+  // aggiornamento all'invio del form
   const handleSubmit = (e) => {
     e.preventDefault();
-    setIdTodo(Number(idtodo) + 1);
+    setIdTodo(Number(idtodo + 1));
     setTodolist([...todolist, { id: idtodo, value: inputValue, doit: false }]);
-    console.log(todolist);
+    setInputValue("");
   };
-  const handleChangeDoIt = (id) => {
-    setTodolist(
-      todolist.map((todo) => {
-        if (todo.id == id) {
-          return { doit: true, ...todo };
-        }
-      })
-    );
-  };
+
+  
+
   return (
     <>
       <h1>To Do List</h1>
       <ul>
-        {todolist.map(({ value, doit, id }) => {
+        {todolist.map((item) => {
           return (
-            <li>
-              <span className="delete">X</span>
-              <span>{value}</span>
-              {/* <span style={doit ? { color: green } : { color: red }}>
-                {doit ? "fatto" : "da fare"}
-              </span> */}
-              {/* {doit ? (
-                <span className="fatto">fatto</span>
-              ) : (
-                <span className="dafare">da fare</span>
-              )} */}
-              <span
-                onClick={handleChangeDoIt(id)}
-                className={doit ? "fatto" : "dafare"}
-              >
-                {doit ? "fatto" : "da fare"}
-              </span>
-            </li>
+            <Item  key={item.id} item={item} todolist={todolist} setTodolist={setTodolist}/>
           );
         })}
       </ul>
+      {/* form */}
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleChange} />
+        <input type="text" onChange={handleChange} value={inputValue}/>
         <input type="submit" value="Aggiungi" />
       </form>
     </>
